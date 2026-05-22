92d78119a99a25decb679ad085a40a440ef017d7
Celine Chen <chenceline626630@gmail.com>
2026-05-22 11:03:37 -0700

    add std and plot

--- calculator.py
+++ calculator.py
@@ -4,3 +4,12 @@ count=len(number)
 mean=total/count
 print("number:", number)
 print("mean:", mean)
+import numpy as np
+import matplotlib.pyplot as plt
+std=np.std(number)
+print("Standard Deviation:", std)
+plt.plot(number)
+plt.title("My Numbers")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.show()
